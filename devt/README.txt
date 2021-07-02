baac6cf6e70eb381348bd72fb8612177205baf78
adhikab <adhikab@miamioh.edu>
2021-07-02 13:12:27 -0400

    removed devt/ folder

--- devt/README.txt
+++
@@ -1,1 +0,0 @@
-The files in this directory are relevant during development of this software slx2mdl. They are not required for production
--- devt/testfunn.m
+++
@@ -1,8 +0,0 @@
-function slxFilepaths = testfunn(slxFolderpath)
-    slxFilesStruct = dir(fullfile(slxFolderpath, '*.slx*')); 
-    slxFilepaths = string.empty; 
-    for i=1:length(slxFilesStruct)
-        slxFilepath = fullfile(slxFolderpath, slxFilesStruct(i).name); 
-        slxFilepaths = [slxFilepaths slxFilepath]; 
-    end
-end
